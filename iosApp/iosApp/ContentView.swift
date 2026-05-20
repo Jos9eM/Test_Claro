16907de0eff540112d84f9dfcd96a354a31d3319
Christopher Guarneros Diaz <chrisgsdiaz@gmail.com>
2026-05-20 15:52:05 -0600

    - Update: se actualiza proyecto para interface en android y apertura en iOS

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,18 +1,20 @@
-import UIKit
 import SwiftUI
+import UIKit
 import ComposeApp
 
-struct ComposeView: UIViewControllerRepresentable {
+struct ComposeRootView: UIViewControllerRepresentable {
+
     func makeUIViewController(context: Context) -> UIViewController {
         MainViewControllerKt.MainViewController()
     }
 
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    }
 }
 
 struct ContentView: View {
     var body: some View {
-        ComposeView()
+        ComposeRootView()
             .ignoresSafeArea()
     }
 }
