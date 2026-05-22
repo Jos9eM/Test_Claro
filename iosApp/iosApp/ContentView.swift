84de56aa6d22c46bffdd893fe33944944be6bc0e
Christopher Guarneros Diaz <chrisgsdiaz@gmail.com>
2026-05-22 09:58:15 -0600

    - se agrega vista de detalle de pelicula desde kotlin

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,7 +1,23 @@
 import SwiftUI
 
+import SwiftUI
+import UIKit
+import ComposeApp
+
+struct ComposeRootView: UIViewControllerRepresentable {
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        // No se necesita actualizar nada por ahora.
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        MovieDetailView(movieId: 2)
+        ComposeRootView()
+            .ignoresSafeArea()
     }
 }
